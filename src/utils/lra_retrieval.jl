0504bf3b2014d357f61e96dd38239a9aa13efc89
alberto.confente <albertoconfente2000@gmail.com>
2025-03-03 13:46:53 +0900

    lra retrieval almost done

--- src/utils/lra_retrieval.jl
+++ src/utils/lra_retrieval.jl
@@ -1,82 +1,154 @@
-using DataFrames, Tables, Flux, DelimitedFiles, Base.Threads, BenchmarkTools
+using DataFrames, Tables, Flux, DelimitedFiles, Base.Threads, BenchmarkTools, ProgressMeter
+using Downloads, GZip, Serialization
 
-function get_vocab_parallel(datasets; min_freq=10)
-    thread_counts = [Dict{Char, Int}() for _ in 1:nthreads()]  # One dictionary per thread
+function get_vocab(datasets; min_freq = 10, tokenizer="char")
+
+    @assert tokenizer=="char" || tokenizer=="subword"
     
-    @threads for i in eachindex(datasets)
-        tid = threadid()
-        local_count = thread_counts[tid]
-        
-        for char in datasets[i]
-            local_count[char] = get(local_count, char, 0) + 1
-        end
-    end
+    T = tokenizer=="char" ? Char : String
+	thread_counts = [Dict{T, Int}() for _ in 1:nthreads()]
 
-    global_count = Dict{Char, Int}()
-    for local_count in thread_counts
-        for (char, freq) in local_count
-            global_count[char] = get(global_count, char, 0) + freq
-        end
-    end
+	@threads for i in eachindex(datasets)
+		tid = threadid()
+		local_count = thread_counts[tid]
 
-    # Filter based on min_freq
-    vocab = [char for (char, freq) in global_count if freq >= min_freq]
+        tokens = tokenizer=="char" ? datasets[i] : split(datasets[i])
 
-    return sort(vocab)
+		for t in tokens
+			local_count[t] = get(local_count, t, 0) + 1
+		end
+	end
+
+	global_count = Dict{T, Int}()
+	for local_count in thread_counts
+		for (t, freq) in local_count
+			global_count[t] = get(global_count, t, 0) + freq
+		end
+	end
+
+	# Filter based on min freq
+	vocab = [t for (t, freq) in global_count if freq >= min_freq]
+
+    println("length of unfiltered vocab: $(length(vocab))")
+    println("length of filtered vocab: $(length(global_count))")
+
+	return sort(vocab)
 end
 
-function get_vocab(datasets; min_freq=10)
-    count = Dict{Char, Int}()
-    for text in datasets
-        for char in text
-            count[char] = get(count, char, 0) + 1
-        end
+function truncate_strings(strs, max_length = 4000)::Matrix{Char}
+    truncated = [length(s) > max_length ? SubString(s, 1, max_length) : s for s in strs]
+    max_len = maximum(length.(truncated))
+    char_matrix = fill(' ', length(truncated), max_len)
+    for i in eachindex(truncated)
+        char_matrix[i, 1:length(truncated[i])] .= collect(truncated[i])
     end
-
-    # Filter out characters with frequency less than min_freq
-    vocab = [char for (char, freq) in count if freq >= min_freq]
-
-    return sort(vocab)
+    return char_matrix
 end
 
-function truncate_strings(strs, max_length=4000)
-    return [length(s) > max_length ? SubString(s, 1, max_length) : s for s in strs]
+function pad(charVector::Matrix{Char}, length::Int)
+    current_length = size(charVector, 2)
+    if current_length < length
+        padding = fill(' ', 1, length - current_length)
+        return hcat(charVector, padding)
+    else
+        return charVector
+    end
 end
 
-function get_lra_retrival(; seq_len=4000, test_percent=0.2, data_to_use_percent=1)
+function download_and_extract_dataset(; download_folder = "download", dataset_folder = "lra_release", url = "https://storage.googleapis.com/long-range-arena/lra_release.gz")
+	gz_file_path = joinpath(download_folder, "$dataset_folder.gz")
+	file_path = joinpath(download_folder, "$dataset_folder")
+	println("Start downloading...")
+	progress = nothing
+	function download_progress_callback(total, now)
+		# not working now
+		if isnothing(progress)
+			progress = Progress(total; desc = "Downloading...", dt = 1.0, output = stderr)
+			println("creating bar")
+		else
+			ProgressMeter.update!(progress, now)
+		end
+	end
 
-    if !isfile("download/lra_release/lra_release/tsv_data/new_aan_pairs.eval.tsv")
-        throw(ErrorException("File was not found"))
-    end
+	#Downloads.download(url, gz_file_path, progress = download_progress_callback)
+	println("Download completed...")
+	println("Start extraction...")
+	GZip.open(gz_file_path) do f_in
+		open(file_path, "w") do f_out
+			write(f_out, read(f_in))
+		end
+	end
+	println("Extraction completed.")
+	rm(gz_file_path)
+end
 
-    if !isfile("download/lra_release/lra_release/tsv_data/new_aan_pairs.train.tsv")
-        throw(ErrorException("File was not found"))
-    end
 
-    train_text_1 = String[]
-    train_text_2 = String[]
-    testY = Float32[]
+function get_lra_retrival(; seq_len = 4000, data_to_use_percent = 1, vocab_folder = "vocab")
+
+	if !isdir("download/lra_release")
+		println("File not found.")
+		download_and_extract_dataset()
+	end
+
+	println("Start loading dataset.")
+
+    num_rows = count(_ -> true, eachline("download/lra_release/lra_release/tsv_data/new_aan_pairs.train.tsv"))
+    train_text_1 = Matrix{Char}(undef, num_rows, 4000)
+    train_text_2 = Matrix{Char}(undef, num_rows, 4000)
+    trainY = Vector{Float32}(undef, num_rows)
+
     open("download/lra_release/lra_release/tsv_data/new_aan_pairs.train.tsv") do file
-        for line in eachline(file)
+
+        for (i, line) in enumerate(eachline(file))
+            
             parts = split(line, '\t')
-            push!(testY, parse(Float32,parts[1]))
-            push!(train_text_1, truncate_strings([parts[4]])[1])
-            push!(train_text_2, truncate_strings([parts[5]])[1])
+            trainY[i] = parse(Float32, parts[1])
+    
+            truncated_1 = truncate_strings([parts[4]])  # Character matrix (1 row)
+            truncated_2 = truncate_strings([parts[5]])  
+            
+            padded_1 = pad(truncated_1, seq_len)
+            padded_2 = pad(truncated_2, seq_len)
+
+            train_text_1[i,:] = padded_1
+            train_text_2[i,:] = padded_2
+
         end
     end
 
-    validation_data = readdlm("download/lra_release/lra_release/tsv_data/new_aan_pairs.eval.tsv", '\t', header=false)
+    println("Training dataset loaded")
 
-    validationY = validation_data[:,1]
-    validation_text_1 = truncate_strings(validation_data[:,4])
-    validation_text_2 = truncate_strings(validation_data[:,5])
+	validation_data = readdlm("download/lra_release/lra_release/tsv_data/new_aan_pairs.eval.tsv", '\t', header = false)
 
-    vocab = get_vocab_parallel(vcat(
-        train_text_1,train_text_2, 
-        validation_text_1, validation_text_2) )
+	validationY = validation_data[:, 1]
+	validation_text_1 = truncate_strings(validation_data[:, 4])
+	validation_text_2 = truncate_strings(validation_data[:, 5])
 
-    return vocab, train_text_1, train_text_2, validation_text_1, validation_text_2
+	println("Validation dataset loaded.")
+
+	if !isfile("$vocab_folder/lra_retrival_vocab.jls")
+		println("Vocab file not found, start computing vocab...")
+		vocab = get_vocab(
+			vcat(train_text_1, train_text_2, validation_text_1, validation_text_2);
+			min_freq = 10)
+		println("Vocab file computed.")
+		open("$vocab_folder/lra_retrival_vocab.jls", "w") do io
+			Serialization.serialize(io, vocab)
+		end
+	else
+		println("Vocab file found")
+		vocab = Serialization.deserialize("$vocab_folder/lra_retrival_vocab.jls")
+	end
+
+    validation_text_1 = map(c -> Int32(findfirst(==(c), vocab)), validation_text_1)
+    validation_text_2 = map(c -> Int32(findfirst(==(c), vocab)), validation_text_2)
+    train_text_1 = map(c -> Int32(findfirst(==(c), vocab)), train_text_1)
+    train_text_2 = map(c -> Int32(findfirst(==(c), vocab)), train_text_2)
+
+	return vocab, train_text_1, train_text_2, trainY, validation_text_1, validation_text_2, validationY
 end
 
-get_lra_retrival()
+vocab, train_text_1, train_text_2, trainY, validation_text_1, validation_text_2, validationY = get_lra_retrival()
+
+
 println("---")
